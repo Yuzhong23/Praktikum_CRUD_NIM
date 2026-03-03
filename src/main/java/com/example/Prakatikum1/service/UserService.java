4ef7012eabbee1da559426ad27a0faa62f878b4b
Yuzhong23 <fatursetia123@gmail.com>
2026-03-03 11:27:12 +0700

    user service java

--- src/main/java/com/example/Prakatikum1/service/UserService.java
+++ src/main/java/com/example/Prakatikum1/service/UserService.java
@@ -2,13 +2,17 @@ package com.example.Prakatikum1.service;
 
 import com.example.Prakatikum1.model.dto.UserAddRequest;
 import com.example.Prakatikum1.model.dto.UserDto;
-
 import java.util.List;
 
 public interface UserService {
-    UserDto AddUser(UserAddRequest request);
+
+    UserDto addUser(UserAddRequest request);
+
     List<UserDto> getAllUser();
+
     UserDto getUserById(String id);
-    UserDto UpdateUser(String id, UserAddRequest request);
-    void DeleteUser(String id);
+
+    UserDto updateUser(String id, UserAddRequest request);
+
+    void deleteUser(String id);
 }
